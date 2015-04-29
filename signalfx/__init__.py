3503a6aa854e7df00b9390b8bd3db8f364fcd656
Maxime Petazzoni <max@signalfx.com>
2015-04-29 10:02:03 -0700

    Implement sending events

--- signalfx/__init__.py
+++ signalfx/__init__.py
@@ -27,6 +27,13 @@ class __BaseSignalFx(object):
         logging.debug('Sending to SignalFx: %s', data)
         return data
 
+    def send_event(self, event_type, dimensions=None, properties=None):
+        data = {'eventType': event_type,
+                'dimensions': dimensions or {},
+                'properties': properties or {}}
+        logging.debug('Sending event to SignalFx: %s', data)
+        return data
+
 
 SignalFxLoggingStub = __BaseSignalFx
 
@@ -42,7 +49,19 @@ class SignalFx(__BaseSignalFx):
     def __init__(self, api_token, api_endpoint=DEFAULT_API_ENDPOINT_URL,
                  ingest_endpoint=DEFAULT_INGEST_ENDPOINT_URL, timeout=1):
         super(SignalFx, self).__init__(
-            self, api_token, api_endpoint, ingest_endpoint, timeout)
+            api_token, api_endpoint, ingest_endpoint, timeout)
+        self._session = requests.Session()
+        self._session.headers.update({
+            'Content-Type': 'application/json',
+            'X-SF-Token': self._api_token
+        })
+        self._session.verify = True
+
+    def _post(self, endpoint, data):
+        return self._session.post(
+            endpoint,
+            data=json.dumps(data),
+            timeout=self._timeout)
 
     def send(self, gauges=None, counters=None):
         """Send the given metrics to SignalFx.
@@ -53,14 +72,28 @@ class SignalFx(__BaseSignalFx):
             counters (list): a list of dictionaries representing the counters
                 to report.
         """
-        data = super(SignalFx, self).send(self, gauges, counters)
+        data = super(SignalFx, self).send(gauges, counters)
         if not data:
             return None
 
-        return requests.post(
-                os.path.join(self._ingest_endpoint, 'datapoint'),
-                headers={'Content-Type': 'application/json',
-                         'X-SF-Token': self._api_token},
-                data=json.dumps(data),
-                verify=True,
-                timeout=self._timeout)
+        return self._post(
+            os.path.join(self._ingest_endpoint, 'datapoint'),
+            data)
+
+    def send_event(self, event_type, dimensions=None, properties=None):
+        """Send an event to SignalFx.
+
+        Args:
+            event_type (string): the event type (name of the event time
+                series).
+            dimensions (dict): a map of event dimensions.
+            properties (dict): a map of extra properties on that event.
+        """
+        data = super(SignalFx, self).send_event(
+            event_type, dimensions, properties)
+        if not data:
+            return None
+
+        return self._post(
+            os.path.join(self._api_endpoint, 'event'),
+            data)
