3e30e98bdd5ea63674264b347c98dd1b8f320ef0
Maxime Petazzoni <max@signalfx.com>
2015-09-30 16:54:57 -0700

    Refactorings to expose only what we want
    Also fixes class naming issue introduced in previous change.

---
+++ signalfx/constants.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2015 SignalFx, Inc. All rights reserved.
+
+# Default Parameters
+DEFAULT_INGEST_ENDPOINT = 'https://ingest.signalfx.com'
+DEFAULT_API_ENDPOINT = 'https://api.signalfx.com'
+DEFAULT_BATCH_SIZE = 300  # Will wait for this many requests before posting
+DEFAULT_TIMEOUT = 1
+
+# Global Parameters
+PROTOBUF_HEADER_CONTENT_TYPE = {'Content-Type': 'application/x-protobuf'}
+JSON_HEADER_CONTENT_TYPE = {'Content-Type': 'application/json'}
