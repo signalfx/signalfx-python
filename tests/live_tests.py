806982c8b400a3b6934221e003c6ce847bd0069c
Maxime Petazzoni <max@signalfx.com>
2016-10-03 11:42:34 +0200

    Fix tests/live_tests for Python 3

--- tests/live_tests.py
+++ tests/live_tests.py
@@ -32,11 +32,16 @@ parser.add_argument('--key')
 parser.add_argument('--value')
 args = parser.parse_args()
 
-if not args.token: args.token = os.environ['SIGNALFX_API_TOKEN']
-if not args.metric_name: args.metric_name = 'fake..metric_to_"test'
-if not args.tag_name: args.tag_name = '"fake_tag_name"~'
-if not args.key: args.key = 'fake_key"%'
-if not args.value: args.value = 'fake_value'
+if not args.token:
+    args.token = os.environ['SIGNALFX_API_TOKEN']
+if not args.metric_name:
+    args.metric_name = 'fake..metric_to_"test'
+if not args.tag_name:
+    args.tag_name = '"fake_tag_name"~'
+if not args.key:
+    args.key = 'fake_key"%'
+if not args.value:
+    args.value = 'fake_value'
 
 FAKE_METRIC_NAME, FAKE_TAG, FAKE_KEY, FAKE_VALUE = \
     args.metric_name, args.tag_name, args.key, args.value
@@ -48,6 +53,7 @@ sD, uD, gD = 'search_dimensions', 'update_dimension', 'get_dimension'
 sfx = signalfx.SignalFx().rest(TOKEN)
 CLIENT_NAME = 'sfx'
 
+
 def test_func(client_name, func_name, sleep_time=5, msg='is being tested!',
               func_args=None, **kwargs):
     return_value = None
@@ -55,18 +61,20 @@ def test_func(client_name, func_name, sleep_time=5, msg='is being tested!',
         try:
             func = eval(client_name + '.' + func_name)
             return_value = func(*func_args, **kwargs)
-            print func_name, msg, '\n', return_value, '\n'
+            print('{} {} -> {}'.format(func_name, msg, return_value))
             time.sleep(sleep_time)
-        except requests.exceptions.HTTPError, e:
-            print func_name, e.message
+        except requests.exceptions.HTTPError as e:
+            print('{} -! {}'.format(func_name, e))
     else:
-        print func_name, 'not available'
+        print('{} is not available'.format(func_name))
     return return_value
 
+
 def apply_sequence(client_name, sequence, f_args, seq_msg=None, **kwargs):
-    print sequence, 'expected behavior is', seq_msg
+    print('{} expected behavior is {}'.format(sequence, seq_msg))
     for item in sequence:
         test_func(client_name, item, func_args=f_args, **kwargs)
+
 
 def main():
     apply_sequence(CLIENT_NAME, [gT, sT, dT, uT, gT, sT, dT, gT, sT, uT,
@@ -81,7 +89,7 @@ def main():
     test_func(CLIENT_NAME, 'update_metric_by_name',
               func_args=[FAKE_METRIC_NAME, 'gauge'], timeout=15)
     r = test_func(CLIENT_NAME, 'search_metric_time_series',
-              func_args=[FAKE_METRIC_NAME], timeout=15)
+                  func_args=[FAKE_METRIC_NAME], timeout=15)
     if r:
         if len(r['results']) > 0:
             mts_id = r['results'][0]['id']
