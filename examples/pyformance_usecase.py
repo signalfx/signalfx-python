456925084f5879f4bdc5ef89bce13044332d8fe7
Charlie Smith <charlie@signalfx.com>
2018-05-01 13:47:44 -0400

    fix typo in pyformance usecase script

--- examples/pyformance_usecase.py
+++ examples/pyformance_usecase.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '..'))
 
 # import the signalfx pyformance library
-import signalfx.pyformance  as pyf # noqa
+import signalfx.pyformance as pyf # noqa
 
 
 if __name__ == '__main__':
