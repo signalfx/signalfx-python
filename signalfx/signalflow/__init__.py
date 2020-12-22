3504fd42da49e0624b2cd86d58c774c088db2729
Hugo G <hgrzeskowiak@atlassian.com>
2020-12-22 17:50:54 +1100

    Add timezone param to signalflow computation requests

--- signalfx/signalflow/__init__.py
+++ signalfx/signalflow/__init__.py
@@ -33,12 +33,13 @@ class SignalFlowClient(object):
 
     def execute(self, program, start=None, stop=None, resolution=None,
                 max_delay=None, persistent=False, immediate=False,
-                disable_all_metric_publishes=None, withDerivedMetadata=None,
-                resolutionAdjustable=None):
+                timezone=None, disable_all_metric_publishes=None,
+                withDerivedMetadata=None, resolutionAdjustable=None):
         """Execute the given SignalFlow program and stream the output back."""
         params = self._get_params(
                 start=start, stop=stop, resolution=resolution,
                 maxDelay=max_delay, persistent=persistent, immediate=immediate,
+                timezone=timezone,
                 disableAllMetricPublishes=disable_all_metric_publishes,
                 withDerivedMetadata=withDerivedMetadata,
                 resolutionAdjustable=resolutionAdjustable)
